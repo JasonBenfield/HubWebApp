0e6ed92960d17e1dbf884eb4cf4aa0ad28e840f2
Jason Benfield <jbenfield108@gmail.com>
2020-10-11 22:44:50 -0400

    Set session id for anon client after authentication

--- Internal/HubWebApi.AuthApi/LoginAction.cs
+++ Internal/HubWebApi.AuthApi/LoginAction.cs
@@ -1,20 +1,25 @@
 ﻿using XTI_App.Api;
 using System.Threading.Tasks;
+using XTI_WebApp;
 
 namespace HubWebApp.AuthApi
 {
     public sealed class LoginAction : AppAction<LoginModel, LoginResult>
     {
-        public LoginAction(Authentication auth)
+        private readonly Authentication auth;
+        private readonly IAnonClient anonClient;
+
+        public LoginAction(Authentication auth, IAnonClient anonClient)
         {
             this.auth = auth;
+            this.anonClient = anonClient;
         }
-
-        private readonly Authentication auth;
 
         public async Task<LoginResult> Execute(LoginModel model)
         {
             var result = await auth.Authenticate(model.UserName, model.Password);
+            anonClient.Load();
+            anonClient.Persist(0, anonClient.RequesterKey);
             return result;
         }
     }
