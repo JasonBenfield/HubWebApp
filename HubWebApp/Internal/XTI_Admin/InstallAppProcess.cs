36a26c8a0f375565d26278964fbbf33fc10d3787
Jason Benfield <jbenfield108@gmail.com>
2022-05-28 16:05:33 -0400

    admin tool should begin and end installation for service apps and console apps. delete previous current installations regardless of appVersionID.

---
+++ HubWebApp/Internal/XTI_Admin/InstallAppProcess.cs
@@ -0,0 +1,8 @@
+﻿using XTI_App.Abstractions;
+
+namespace XTI_Admin;
+
+internal interface InstallAppProcess
+{
+    Task Run(string publishedAppDir, AdminInstallOptions adminInstOptions, AppVersionKey installVersionKey);
+}
