ce5be8b4f87a7e6e8af69484f8f6da7ca8c0ec05
jbenfield108@gmail.com <jbenfield108@gmail.com>
2023-02-05 20:58:07 -0500

    Version V1416

--- HubWebApp/Lib/XTI_HubAppClient/HubAppClientVersion.cs
+++ HubWebApp/Lib/XTI_HubAppClient/HubAppClientVersion.cs
@@ -12,7 +12,7 @@ public sealed class HubAppClientVersion
         string value;
         if (hostEnv.IsProduction())
         {
-            value = "V1408";
+            value = "V1416";
         }
         else
         {
